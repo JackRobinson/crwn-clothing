d214fccb0877cb187e17c83692f2bd10a51727ad
Maxim Kazimirski <maxim.kazimirski@viv.health>
2020-06-27 00:12:46 +0300

    - Another commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,10 @@ import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 import { persistStore } from "redux-persist";
 
-const middlewares = [logger];
+const middlewares = [];
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(
